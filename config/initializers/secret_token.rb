6398e29382c275d41165eee738364aeaf61eb341
roberto.cortes <roberto.cortes@virtus.com.mx>
2010-11-05 22:37:59 -0600

    Commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Project::Application.config.secret_token = 'c835c75cfd9cab1eab0b090930bfb04f7e589a4d19f30e1c5169b1b45cffc5bd329f89f4fc2c7948f5b62e3cc80bac6f8a2d43830f31d0889064bef33a03471d'
